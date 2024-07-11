a15ecddaac74edf6aed4466ff9ee2577f6c67d6d
NexGenIno <165064417+NexGenIno@users.noreply.github.com>
2024-07-10 23:13:52 -0600

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLDxPdItAb-6s_TDHMh7-wKGEtpn6xc3Y",
-  authDomain: "vod-koinonia.firebaseapp.com",
-  projectId: "vod-koinonia",
-  storageBucket: "vod-koinonia.appspot.com",
-  messagingSenderId: "819898281723",
-  appId: "1:819898281723:web:7d3337df625c42c8604a5e",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
